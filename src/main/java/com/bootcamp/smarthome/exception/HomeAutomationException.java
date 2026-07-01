cd95f0b083421638909596f0a854a004d050af47
karina <karinasteina@gmail.com>
2026-07-01 10:48:47 +0300

    task 1 - completed

---
+++ src/main/java/com/bootcamp/smarthome/exception/HomeAutomationException.java
@@ -0,0 +1,7 @@
+package com.bootcamp.smarthome.exception;
+
+public class HomeAutomationException extends Exception{
+    public HomeAutomationException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/com/bootcamp/smarthome/exception/InvalidCommandException.java
@@ -0,0 +1,9 @@
+package com.bootcamp.smarthome.exception;
+
+public class InvalidCommandException extends HomeAutomationException{
+    public InvalidCommandException(String message) {
+        super(message);
+
+    }
+
+}
